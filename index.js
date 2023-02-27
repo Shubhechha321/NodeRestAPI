ffca301c2eb0aeb34973654cc6a9a95e23e3d32f
Shubhechha Sanyal <shubhechha.321@gmail.com>
2023-02-27 19:19:55 +0530

    bcrypt removed

--- index.js
+++ index.js
@@ -24,9 +24,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.use("/", (req, res) => {
-  res.json({ message: "Hello Vercel" });
-});
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", checkToken, userRoute);
 app.use("/api/posts", postRoute);
